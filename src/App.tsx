f04a01747f59861e88c455f0faf9eaab7d8b5828
Zulu <zulu.devs@gmail.com>
2024-03-27 14:11:16 +0100

    learning react+typescript

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import './App.css'
+import { Button } from './components/props/Button'
+import { Container } from './components/props/Container'
+import { Greet } from './components/props/Greet'
+import { Heading } from './components/props/Heading'
+import { Input } from './components/props/Input'
+import { Oscar } from './components/props/Oscar'
+import { Person } from './components/props/Person'
+import { PersonList } from './components/props/PersonList'
+import { Status } from './components/props/Status'
+import { LoggedIn } from './components/state/LoggedIn'
+
+function App() {
+   const personName = {
+    first: 'Bruce',
+    last: 'Wayne'
+  }
+
+  const nameList = [
+    {
+      first: 'Bruce',
+      last: 'Wayne'
+    },
+    {
+      first: 'Clark',
+      last: 'Kent'
+    },
+    {
+      first: 'Princess',
+      last: 'Diana'
+    }
+  ]
+
+  return (
+    <>
+     <div className='App'>
+     {/* <Greet name='vishwas' messageCount={23} isLoggedIn={true}/>
+     <Person name={personName}/>
+    <PersonList names={nameList} />
+      <Status status='loading' />
+     <Heading>Placeholder text</Heading>
+     <Oscar>
+      <Heading> Placeholder text</Heading>
+     </Oscar>
+      <Greet name='Zido'  isLoggedIn={false} />
+    <Button handleClick={(event, id) => {
+      console.log('Button Clicked', event, id);
+     }} />
+     <Input value='' handleChange={event => console.log(event)} />
+      <Container styles={{ border: '1px solid black', padding: '1rem' }} /> */}
+      <LoggedIn />
+     </div>
+    </>
+  )
+}
+
+export default App
